9d80ee35557d5a9638dc061d1c6343c5d01a05cb
alexandre362 <alexandre@charriol.com>
2026-03-23 11:08:58 +0100

    Create shopify-collection-widget.js
    To connect collection Amber's design in Shopify to the website's partner

---
+++ shopify-collection-widget.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  storeUrl: 'https://charriol-geneve.myshopify.com',
+  accessToken: 'b54f4f45a4e83458cd38eb0b5e15fc85',
+  collectionHandle: 'ambers-design',
+  
+  // GESTION DEVISES
+  enableCurrencyConversion: true,    // ← Mettez à true
+  sourceCurrency: 'CHF',
+  targetCurrency: 'USD',
+  exchangeRate: 1.08,                // ← Mettez le taux actuel (XE.com)
+};
